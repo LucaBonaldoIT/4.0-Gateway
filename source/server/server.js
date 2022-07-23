7d3d75038a3a121ed1733ed79564d5f9e4643880
LucaBonaldoIT <lucabonaldoit@gmail.com>
2022-07-23 18:45:18 +0200

    Initial project structure concept
    Bare bones for the structure of the project. Feature will be implemented in the future. At the moment the project is still in an embryo stage.

---
+++ source/server/server.js
@@ -0,0 +1,12 @@
+/*
+
+    Modbus to OPC-UA server
+
+*/
+
+`use strict`;
+
+var modbus = require('jsmodbus');
+var opcua = require("node-opcua");
+
+// Todo - Add Modbus to OPC-UA implementation
---
+++ source/web/web.py
@@ -0,0 +1,9 @@
+"""
+
+    Web application with Flask
+
+"""
+
+from flask import Flask
+
+# Todo - Add web application implementation
