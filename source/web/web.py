b0cd02426ad0c4e82f45ee826482c89aba7925d9
LucaBonaldoIT <lucabonaldoit@gmail.com>
2022-07-25 16:24:26 +0200

    Added web application (WIP)
    Small setup

--- source/web/web.py
+++ source/web/web.py
@@ -4,6 +4,14 @@
 
 """
 
-from flask import Flask
+from flask import Flask, render_template
 
 # Todo - Add web application implementation
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return render_template('index.html')
+
+app.run()
